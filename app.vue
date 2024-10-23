4907668aaa0fed009420e9c0188be37e28f52ef2
simpaul13 <28johnpaulsim28@gmail.com>
2024-10-23 17:04:27 +0800

    WIP

--- app.vue
+++ app.vue
@@ -10,3 +10,22 @@
     <NuxtWelcome /> -->
   </div>
 </template>
+<script setup>
+import { onMounted } from 'vue';
+
+const setLightMode = () => {
+  document.documentElement.setAttribute('data-theme', 'light');
+  localStorage.setItem('theme', 'light');
+};
+
+const setDarkMode = () => {
+  document.documentElement.setAttribute('data-theme', 'dark');
+  localStorage.setItem('theme', 'dark');
+};
+
+onMounted(() => {
+  // Check if user previously selected a theme
+  const savedTheme = localStorage.getItem('theme') || 'light'; // Default to light mode
+  document.documentElement.setAttribute('data-theme', savedTheme);
+});
+</script>
