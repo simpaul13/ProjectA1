c185060e768a6e41dc6805cf589a129d099546f7
simpaul13 <28johnpaulsim28@gmail.com>
2024-10-27 17:39:49 +0800

    WIP

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,41 +2,18 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  build: {
+    extractCSS: true,
+    optimizeCSS: true,
+  },
   components : [
     { path: '~/components', pathPrefix: false },
   ],
-  modules: ['@nuxt/ui', '@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', '@nuxtjs/seo'],
+  modules: ['@nuxt/ui', '@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
   googleFonts: {
     families: {
       'Fredoka': [400, 500, 700],
     },
     display: 'swap',
   },
-  app: {
-    head: {
-      title: 'Project A1',
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: '' },
-        { name: 'format-detection', content: 'telephone=no' },
-      ],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-    },
-  },
-  ssr: true,
-  seo: {
-    name: 'Project A1',
-    description: 'Project A1',
-    keywords: ['Project A1', 'Nuxt 3', 'Tailwind CSS'],
-    ogTitle: 'Project A1',
-    ogDescription: 'Project A1',
-    openGraph: {
-      title: 'Project A1',
-      description: 'Project A1',
-      url: 'https://project-a1.pages.dev',
-      type: 'website',
-      locale: 'en',
-    }
-  }
 })
