551f97bf2a58da3497af44b42c9c9813b5f29110
simpaul13 <28johnpaulsim28@gmail.com>
2024-10-27 17:44:28 +0800

    SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,4 +16,17 @@ export default defineNuxtConfig({
     },
     display: 'swap',
   },
+  app: {
+    head: {
+      title: 'Project A1',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
+  ssr: true,
 })
